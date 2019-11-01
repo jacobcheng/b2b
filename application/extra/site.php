07a1050cb433bd7b349f46058fb21e04f4953b6e
Jacob Zheng <im.jacobz@outlook.com>
2019-11-01 17:31:15 +0800

    管理员郑家手机号码中文名

--- application/extra/site.php
+++ application/extra/site.php
@@ -15,18 +15,17 @@ return array (
   'fixedpage' => 'dashboard',
   'categorytype' => 
   array (
-    'default' => 'Default',
-    'page' => 'Page',
-    'article' => 'Article',
-    'test' => 'Test',
+    'product' => 'Product',
+    'blog' => 'blog',
   ),
   'configgroup' => 
   array (
     'basic' => 'Basic',
+    'company' => 'Company',
+    'website' => 'Website',
     'email' => 'Email',
     'dictionary' => 'Dictionary',
     'user' => 'User',
-    'example' => 'Example',
   ),
   'mail_type' => '1',
   'mail_smtp_host' => 'smtp.qq.com',
@@ -35,4 +34,19 @@ return array (
   'mail_smtp_pass' => 'password',
   'mail_verify_type' => '2',
   'mail_from' => '10000@qq.com',
+  'company_name' => '东莞市龙威电子科技有限公司',
+  'company_name_en' => 'Longwei Instruments (HK) Co., Limited',
+  'address' => '中国广东省东莞塘厦林村工业区多仔岭49/50/45栋',
+  'address_en' => 'Building 49/50/45, Duozailing, Lincun Industrial Area, Tangxia, Dongguan, Guangdong, China',
+  'postal_code' => '523711',
+  'tel' => '+86 769 87722745',
+  'fax' => '+86 769 82618128',
+  'website' => 'https://longweielec.com',
+  'logo' => '/uploads/20191101/02dc6f1396e98c2926504fd77eacc24a.svg',
+  'favicon' => '/uploads/20191101/5b26af6eaea4a558e43ec8288338613e.png',
+  'multi_lang' => 
+  array (
+    'en' => 'English',
+    'zh' => 'Chinese',
+  ),
 );
